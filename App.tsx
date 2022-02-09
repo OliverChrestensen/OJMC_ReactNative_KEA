0e90c722df57bf7ce9cd3ed18031c510db2cd33c
OliverChrestensen <oliver.chrestensen@hotmail.com>
2022-02-09 11:40:37 +0100

    second 2

--- App.tsx
+++ App.tsx
@@ -1,20 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import TodoScreen from './screens/TodoScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tfefefsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <TodoScreen />
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
