714545a55f0bcdbbb8e0d905cc2054892e28eeb3
OliverChrestensen <oliver.chrestensen@hotmail.com>
2022-03-23 08:39:49 +0100

    d.23/03

--- App.tsx
+++ App.tsx
@@ -1,8 +1,26 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
-import TodoScreen from './screens/TodoScreen';
+import Screen1 from './screens/Screen1';
+import Screen2 from './screens/Screen2';
+import Screen3 from './screens/Screen3';
 
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 export default function App() {
+
   return (
-    <TodoScreen />
+    <NavigationContainer>
+      < Tab.Navigator 
+
+
+      <Stack.Navigator>
+        <Stack.Screen name="Screen1" component={Screen1} />
+        <Stack.Screen name="Screen2" component={Screen2} />
+        <Stack.Screen name="Screen3" component={Screen3} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
